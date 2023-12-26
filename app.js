77112487f9668b2e1a1f47e6f142d0923b48fe6c
Olga <olga_lesechko@mail.ru>
2023-12-26 16:17:04 +0500

    first

---
+++ app.js
@@ -0,0 +1,11 @@
+
+let count = +prompt('Введите число бананов');
+
+for(let i = 1; i <= count; i++){
+    if(i === 1){
+    console.log(i + ' banana');
+    }else{
+        console.log(i + ' bananas');
+    }
+}
+
